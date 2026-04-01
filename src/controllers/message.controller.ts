580870267d60cf6b10fd57d40d9e96d037d8b965
Rajnish Nath <rnish612@gmail.com>
2026-04-01 06:32:05 +0530

    implement live ui update on message unsend

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -59,3 +59,53 @@ export const sendTextMessage = asyncHandler(
     res.status(200).json(response);
   },
 );
+export const unsendMessage = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const userId = req.user._id;
+    const messageId = req.query._id;
+
+    const message = await Message.findById(messageId).lean();
+
+    if (!message) throw new AppError("Message not found", 404);
+    if (message.senderId.toString() !== userId.toString()) {
+      throw new AppError("You are not the owner of this message", 403);
+    }
+    const lastMessage = await Message.findOne({
+      chatId: message.chatId,
+      _id: { $ne: message._id },
+    })
+      .sort({ createdAt: -1 })
+      .lean();
+    const updatedChat = await Chat.findByIdAndUpdate(
+      message.chatId,
+      {
+        $set: {
+          lastMessage: lastMessage?._id || null,
+          lastMessageAt: lastMessage ? lastMessage.createdAt : null,
+        },
+      },
+      { returnDocument: "after" },
+    )
+      .populate("lastMessage")
+      .populate("participants.userId", "_id fullName profilePic")
+      .lean();
+    updatedChat.participants.forEach(
+      (participant: { userId: { _id: string } }) => {
+        io.to(participant.userId._id.toString()).emit("chat-update", {
+          chatId: lastMessage.chatId,
+          updatedChat,
+        });
+      },
+    );
+    await Message.deleteOne({ _id: messageId });
+    io.to(message.chatId.toString()).emit("message-unsent", {
+      messageId,
+    });
+    const response: ApiResponse<null> = {
+      success: true,
+      data: message._id,
+      message: "Message deleted",
+    };
+    res.status(200).json(response);
+  },
+);
--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -3,12 +3,14 @@ import { verifyToken } from "../middleware/auth.middleware";
 import {
   getMessages,
   sendTextMessage,
+  unsendMessage,
 } from "../controllers/message.controller";
 const router = express.Router();
 const sendRouter = express.Router();
 router.use("/send", sendRouter);
 
 router.route("/get-messages").get(verifyToken, getMessages);
+router.route("/unsend").post(verifyToken, unsendMessage);
 
 sendRouter.route("/text-message").post(verifyToken, sendTextMessage);
 export default router;
